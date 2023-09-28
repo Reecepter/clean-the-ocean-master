41ce947c5b804427df6ed324fb0e088cb28856ee
Reecepter <rfreem57@students.kennesaw.edu>
2023-09-28 19:13:18 -0400

    9-28_final

---
+++ Assets/FishScripts/Flock.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+    public FlockManager myManager;
+    
+    float speed;
+    bool turning = false;
+    public bool avoid;
+    public int avoidDistance;
+
+    //Vector3 direction;
+   
+    //bool ao = (myManager<avoidObstacles>);
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+        
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2);
+
+        RaycastHit hit = new RaycastHit();
+        Vector3 direction = Vector3.zero;
+
+
+
+        if (avoid)
+        {
+            
+            if (!b.Contains(transform.position))
+            {
+                turning = true;
+                direction = myManager.transform.position - transform.position;
+            }
+            else if (Physics.Raycast(transform.position, this.transform.forward * avoidDistance, out hit))
+            {
+                turning = true;
+                direction = Vector3.Reflect(this.transform.forward, hit.normal);
+                Debug.DrawRay(this.transform.position, this.transform.forward * avoidDistance, Color.red);
+            }
+            else
+                turning = false;
+        }
+
+        if(!avoid)
+        {
+
+            if (!b.Contains(transform.position))
+            {
+                turning = true;
+                direction = myManager.transform.position - transform.position;
+            }
+
+            else
+                turning = false;
+
+        }
+
+        if(turning)
+        {
+           // Vector3 direction = myManager.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            if (Random.Range(0, 100) < 20)
+                ApplyRules();
+        }
+
+
+       
+
+
+
+       // ApplyRules();
+        transform.Translate(0, 0, Time.deltaTime * speed);
+		
+	}
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = myManager.allFish;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.0f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in gos)
+            if (go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (nDistance <= myManager.neighborDistance)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+
+                    if (nDistance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+
+        if (groupSize > 0)
+        {
+            vcenter = vcenter / groupSize + (myManager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcenter + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+    
+}
---
+++ Assets/FishScripts/FlockManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour {
+
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(10, 10, 10);
+    public Vector3 flockSize = new Vector3(5, 5, 5);
+    public int flockChangeFrequency = 10;
+    [Header("(Can Choose 1-1000 for flock change frequency)")]
+    [Header("")]
+    public Vector3 goalPos;
+    Vector3 center;
+    //public  bool avoidObstacles;
+
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighborDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+
+        goalPos = this.transform.position;
+        center = this.transform.position;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Random.Range(0,1000)< flockChangeFrequency)
+        goalPos = this.transform.position + new Vector3(Random.Range(-flockSize.x, flockSize.x), Random.Range(-flockSize.y, flockSize.y), Random.Range(-flockSize.z, flockSize.z));
+        center = this.transform.position;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(center, swimLimits);
+        Gizmos.DrawCube(goalPos, flockSize);
+    }
+}
