e65a1e82f03f6df50bc86275ca5388225178465c
Reecepter <rfreem57@students.kennesaw.edu>
2023-10-17 18:00:22 -0400

    10-17

---
+++ Assets/Scripts/MoveStopStart.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWS;
+using UnityEngine.InputSystem;
+using System;
+
+public class MoveStopStart : MonoBehaviour
+{
+    public splineMove playerSpline;
+    public InputActionReference lTriggerInputActionRefence;
+    private bool pathPaused = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        lTriggerInputActionRefence.action.performed += PauseMovement;
+    }
+
+    private void PauseMovement(InputAction.CallbackContext context)
+    {
+        if (pathPaused == false)
+        {
+            playerSpline.Pause();
+            pathPaused = true;
+        }
+        else
+        {
+            playerSpline.Resume();
+            pathPaused = false;
+        }
+    }
+}
