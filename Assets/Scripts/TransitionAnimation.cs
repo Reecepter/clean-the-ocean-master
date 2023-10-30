8bd28e857bce4a8eb2e59009a47877f45e0ee2a5
Reecepter <rfreem57@students.kennesaw.edu>
2023-10-30 16:53:11 -0400

    10-30

---
+++ Assets/Scripts/TransitionAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionAnimation : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+        anim.SetBool("StartAnimation", true);
+    }
+    public void StopAnimation()
+    {
+        anim.SetBool("StartAnimation", false);
+    }
+}
--- Assets/Scripts/TurnUnhealthy.cs
+++ Assets/Scripts/TurnUnhealthy.cs
@@ -7,23 +7,26 @@ public class TurnUnhealthy : MonoBehaviour
     // Start is called before the first frame update
     // Blends between two materials
 
-    public Material material1;
-    public Material material2;
+    //public Material material1;
+    //public Material material2;
     float duration = 2.0f;
-    Renderer rend;
+    //Renderer rend;
 
-    bool allowTrans = false;
+    float unhealthyStrength;
 
-    void Start()
-    {
-        rend = this.GetComponent<Renderer>();
 
-        // At start, use the first material
-        //rend.material = material1;
-        material1 = rend.material;
-        StartCoroutine(LerpMaterial());
+    //bool allowTrans = false;
 
-    }
+    //void Start()
+    //{
+    //    //rend = this.GetComponent<Renderer>();
+
+    //    // At start, use the first material
+    //    //rend.material = material1;
+    //    //material1 = rend.material;
+    //    StartCoroutine(LerpMaterial());
+
+    //}
 
     void Update()
     {
@@ -31,17 +34,26 @@ public class TurnUnhealthy : MonoBehaviour
             //rend.material.Lerp(material1, material2, Time.deltaTime);
         // ping-pong between the materials over the duration
     }
+    public void StartUnhealthy()
+    {
+        StartCoroutine(LerpMaterial());
+    }
 
-    IEnumerator LerpMaterial()
+    public IEnumerator LerpMaterial()
     {
         float time = 0;
+
+        Material seaweedShader = GetComponent<Renderer>().material;
+
         while (time < duration)
         {
-            material1.Lerp(material1, material2, time / duration);
             time += Time.deltaTime;
+
+            unhealthyStrength = Mathf.Lerp(0, 1, time / duration);
+            seaweedShader.SetFloat("_LerpAmount", unhealthyStrength);
+            
             yield return null;
         }
-        rend.material = material2;
     }
 
 
