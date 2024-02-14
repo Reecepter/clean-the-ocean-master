34531d3664bc6c13e60910402a9a9faeffb81640
Reecepter <rfreem57@students.kennesaw.edu>
2024-02-14 17:40:18 -0500

    2/14 (MOVEMENT AND TASKS)
    Added in ability for player to move around freely. I also added in the task system to the healthy to unhealthy scene. Will add task system to unhealthy scene with next commit.

---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class TaskManager : MonoBehaviour
+{
+    public GameObject[] taskIcons = new GameObject[7];
+    public GameObject[] taskTriggers = new GameObject[7];
+    private int currentTask = 0;
+    [SerializeField]
+    private UnityEvent _NextLevel;
+    
+    public void NextTask()
+    {
+        taskIcons[currentTask].SetActive(false);
+        taskTriggers[currentTask].SetActive(false);
+        currentTask++;
+        if(currentTask >= taskIcons.Length)
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            if (!currentScene.name.Equals("_mainScene_unhealthy"))
+            {
+                _NextLevel?.Invoke();
+            }
+        }
+        else
+        {
+            taskIcons[currentTask].SetActive(true);
+            taskTriggers[currentTask].SetActive(true);
+        }
+    }
+}
